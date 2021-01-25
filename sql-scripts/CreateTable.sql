18c5645fbb4d5595c499daea9f19f677d1255d2e
Elam Day <elam.day.friedland@gmail.com>
2021-01-25 11:57:40 -0800

    user time

--- sql-scripts/CreateTable.sql
+++ sql-scripts/CreateTable.sql
@@ -1,7 +1,6 @@
 CREATE TABLE sensdata (
 	time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	id int,
-	name varchar(15),
+	sensorname varchar(15) NOT NULL,
 	temp int,
 	humidity int,
 	pressure int,
@@ -9,5 +8,18 @@ CREATE TABLE sensdata (
 	NO2 FLOAT,
 	NH3 FLOAT,
 	CO FLOAT,
-	CO2 int
+	CO2 int,
+	FOREIGN KEY (sensorname) REFERENCES sensors(sensorname)
+);
+CREATE TABLE sensors (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	BOOLEAN isup DEFAULT false,
+	sensorname varchar(15) NOT NULL UNIQUE,
+	belongsto varchar(15) NOT NULL,
+	FOREIGN KEY (belongsto) REFERENCES users(username)
+);
+CREATE TABLE users (
+	username varchar(15) NOT NULL UNIQUE PRIMARY KEY,
+	password varchar(20) NOT NULL,
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 );
